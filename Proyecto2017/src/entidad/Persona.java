77229a427c5650efe8d87008d69584e180d34c04
jimenasantiago <jimmlala@hotmail.com>
2017-01-16 19:27:38 -0300

    agregado de clases capa entidad - segun MD

--- Proyecto2017/src/entidad/Persona.java
+++ Proyecto2017/src/entidad/Persona.java
@@ -1,5 +1,52 @@
 package entidad;
 
+import java.util.*;
+
 public class Persona {
 
+	private String apellido;
+	private String nombre;
+	private int dni;
+	private Date fecha_nacimiento;
+	private String sexo;
+	private String email;
+	
+	
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+	public Date getFecha_nacimiento() {
+		return fecha_nacimiento;
+	}
+	public void setFecha_Nacimiento(Date fecha_nacimiento) {
+		this.fecha_nacimiento = fecha_nacimiento;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
